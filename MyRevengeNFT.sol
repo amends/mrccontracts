f027b7c0355da51a852f121681e7d5118063152d
amends <85320590+amends@users.noreply.github.com>
2021-09-17 17:44:24 -0700

    add URI; final commit

--- MyRevengeNFT.sol
+++ MyRevengeNFT.sol
@@ -16,18 +16,18 @@ contract MyRevengeNFT is ERC721Enumerable, ReentrancyGuard {
     address public constant deadAddress = 0x000000000000000000000000000000000000dEaD;
 
     constructor(address _LoveToken) ERC721("MyRevengeNFT", "REVENGENFT") {
-        LoveToken = _LoveToken; //We pull in the LoveToken details
+        LoveToken = _LoveToken; 
     }
 
     function mintNFT() public {
-        require(totalSupply() < 10000, "All NFTs Minted!"); //Max Supply 10,000 NFTs
-        Love(LoveToken).transferFrom(msg.sender, deadAddress, 10e18); //Burns LOVE tokens. Oh no!!
-        uint256 tokenID = totalSupply() // Increment the supply. Starting at 0
-        _mint(msg.sender, tokenID); // Mint the NFT
+        require(totalSupply() < 10000, "All NFTs Minted!"); 
+        Love(LoveToken).transferFrom(msg.sender, deadAddress, 10e18); //Burns 10 LOVE tokens. Oh no!!
+        uint256 tokenID = totalSupply();
+        _mint(msg.sender, tokenID); 
 
     }
     function tokenURI() public pure returns (string memory) {
-    string memory URI = "https://imgur.com/iG93Nbr.jpg"; //This will be changed to ipfs link once final art is in.
+    string memory URI = "https://ipfs.io/ipfs/QmZ71VbRiYeS3aYYS9ugfAmHnLiK5QzT22ttcvc1PGvY8M";
     return URI;
     }
 }
